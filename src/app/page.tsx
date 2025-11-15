ce153b659072d89b1e7a1670f1b1c2842f222daf
Trọng Đoàn Ngọc <trong.doanngoc2023@gmail.com>
2025-11-15 21:30:28 +0700

    Integrate TRPC and React Query for data fetching; add new API routes and client setup. Update dependencies in package.json and bun.lock.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { caller } from "@/trpc/server";
 
-const Page = () => {
+const Page = async () => {
+  const users = await caller.getUsers();
   return (
     <div>
       <Button>Click me</Button>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   );
 };
