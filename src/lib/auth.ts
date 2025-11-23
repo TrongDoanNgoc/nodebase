869de200ed4e0910c536be3f27d0352ae585073a
Trọng Đoàn Ngọc <trong.doanngoc2023@gmail.com>
2025-11-23 12:03:03 +0700

    feat: integrate Polar authentication and subscription management; update sidebar for subscription checks; add new hooks for subscription state; enhance auth client with Polar SDK.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,14 @@
 import { betterAuth } from "better-auth";
 import prisma from "@/lib/db";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
+import { polarClient } from "@/lib/polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +18,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "c4d68b33-b3b7-4884-be51-4bcefe916894",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
